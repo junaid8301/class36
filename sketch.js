a00ef4f42850a0034924e6a417da4d32c06eb38e
junaid8301 <85234771+junaid8301@users.noreply.github.com>
2021-07-31 15:55:29 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,25 @@
+var hypnoticBall, database;
+var position;
+
+
+function setup(){
+  database = firebase.database();
+  console.log(database);
+  createCanvas(500,500);
+  f1 = new Form()
+  f1.display()
+
+  
+}
+
+function draw(){
+  background("white");
+  
+   
+  
+}
+
+
+
+
+
